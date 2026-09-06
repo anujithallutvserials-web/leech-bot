1f5c0fcc89d996205136de803e436021624cc80d
anujithallutvserials-web <anujithallutvserials@gmail.com>
2026-09-06 14:01:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -538,4 +538,10 @@ async def main():
     await idle()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    
+    loop.run_until_complete(main())
